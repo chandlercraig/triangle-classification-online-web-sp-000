6c699d1b74c2fa7294f85b313472f368cac502d0
Chandler Craig <hellochandlercraig@gmail.com>
2020-12-13 23:16:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,26 +1,26 @@
 class Triangle
-  attr_accessor :side1, :side2, :side3, :side_arr
+  attr_accessor :side_arr
 
-  def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+  @side_arr = []
+
+  def initialize (side1, side2, side3)
     @side_arr = [side1, side2, side3]
+    @sides.sort!
   end
 
 
   def kind
-    if @side1 == @side2 && @side2 == @side3
+    if @sides.any?{|side| side <= 0} || ((@sides[0] + @sides[1]) <= @sides[2])
+      raise TriangleError
+    elsif @sides.uniq.length == 1
       :equilateral
-    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+    elsif @sides.uniq.length == 2
       :isosceles
     else
       :scalene
     end
-  # raise TriangleError if @side_arr.each {|s| s <= 0}
-  # raise TriangleError if @side1+@side2<=@side3 || @side2+@side3<=@side1 || @side1+@side3<=@side2
-  end
-
+ end
+ 
   class TriangleError < StandardError
   end
 
